93ae76c826f60646822e23bac4bdde1a23f03632
flaviolrita <frita@beesion.com>
2020-11-17 03:48:52 +0100

    avance en movimiento y rotacion

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 
 let game;
-let pieces=[];
+let currentPiece;
 let config;
+let mat;
 
 async function setup() {  
   //angleMode(DEGREES);
@@ -10,20 +11,39 @@ async function setup() {
   let age =  urlParams.has('age')?urlParams.get('age'):4;
   let level = urlParams.has('level')?urlParams.get('level'):1;
   config = await $.ajax({url: '/age/'+age+'/level/'+level+'/config',type: 'GET'});
-
   const canvas = createCanvas(config.screen.width, config.screen.high);
   canvas.parent('#canvasHolder');
   //game = new Game(config);,
 
-  pieces.push(new SquarePiece(0,0,false,0,1));
-  pieces.push(new TPiece(5,0,true,0,1));
+  mat = new Array(config.rows);
+  for(let i=0;i<config.rows;i++){
+    mat[i]=new Array(config.cols);
+    for(let j=0;j<config.rows;j++){
+      mat[i][j]="";
+    }
+  }
+
+  //pieces.push(new SquarePiece(0,0,false,0,1));
+  currentPiece = new TPiece(5,0,0,1);
 }
 async function draw() {
     background(0);
-    for(let i=0;i<pieces.length;i++){
-      pieces[i].updatepPosition(); 
-      pieces[i].draw();  
-    }      
+    if(currentPiece)      
+       currentPiece.draw(); 
+
+    if(mat){
+      for(let i=0;i<config.rows;i++){
+        for(let j=0;j<config.cols;j++){
+          if(mat[i][j]!=""){
+            fill(mat[i][j]);
+            rect(j*config.pixelSize,i*config.pixelSize, config.pixelSize , config.pixelSize );
+          }
+        }
+      }
+    }
+           
+      
+          
 }
 
 
@@ -31,7 +51,7 @@ async function draw() {
 
 class Piece
 {
-    constructor(x,y,current,angle,speed,shape,color){
+    constructor(x,y,angle,speed,shape,color){
       this.x=x;
       this.y=y;
       this.size = config.pixelSize;
@@ -39,21 +59,57 @@ class Piece
       this.color = color;
       this.speed = speed;
       this.shape = shape;
-      this.shape_lines = this.shape.split('\r\n'); 
-      this.shape_high = this.shape_lines.length;
-      this.shape_width = this.shape_lines[0].length;
-      this.current = current;
+      let lines = this.shape.split('\r\n'); 
+      this.shape_cols = lines.length;
+      this.shape_rows = lines[0].length;
+      this.shape_mat= new Array(this.shape_cols);
+      for(let i=0;i<this.shape_cols;i++){
+        let line = lines[i];
+        this.shape_mat[i] = new Array(this.shape_rows);
+        for(let j=0;j<this.shape_rows;j++){
+          this.shape_mat[i][j] = line[j];
+        }
+      } 
+      this.try= 0;
+      
       
     }
     draw(){
       
-      //rotate(90);       
-     
-      for(let i=0;i<this.shape_lines.length;i++){
-        let line = this.shape_lines[i];
-        for(let j=0;j<line.length;j++){
-          fill(this.color);           
-          rect((this.x+j)*this.size,(this.y+i)*this.size, this.size , this.size );
+      this.try++;
+      if(this.try * this.speed >10){
+        this.updatepPosition();
+        this.try=0;
+      }
+      
+      //  #
+      //  ##
+      //  #
+
+      //  ###
+      //   #
+      
+    
+
+      for(let i=0;i<this.shape_cols;i++){       
+        for(let j=0;j<this.shape_rows;j++){
+          if(this.shape_mat[i][j]=="#"){
+            fill(this.color);
+            switch(this.angle) {
+              case 0: 
+                rect((this.x+j)*this.size,(this.y+i)*this.size, this.size , this.size ); 
+                break; 
+              case 90: 
+                rect((this.x+i)*this.size,(this.y+j)*this.size, this.size , this.size ); 
+                break;
+              case 180: 
+                rect((this.x+j)*this.size,(this.y+i)*this.size, this.size , this.size ); 
+                break;
+              case 270: 
+                rect((this.x+i)*this.size,(this.y+j)*this.size, this.size , this.size );  
+                break; 
+            } 
+          }
         } 
       }
      
@@ -63,28 +119,27 @@ class Piece
      
 
     updatepPosition(){
-
-      if(this.current){
-        let _x=0;
-        let _y=0;
-        if(keyIsDown(LEFT_ARROW)){
-          _x-=this.speed;
-        }
-        else if(keyIsDown(RIGHT_ARROW)){
-          _x+=this.speed;
-        }
-        else if(keyIsDown(UP_ARROW)){
-          _y-=this.speed; 
-        }
-        //else if(keyIsDown(UP_ARROW))this.angle+=90;//   _rotate(90); //REVISAR  0 90 180 270
-        else if(keyIsDown(DOWN_ARROW)){
-            _y+=this.speed;
-        }
-        let next_x = parseInt(this.x + _x);
-        let next_y =  parseInt(this.y + _y); 
-        if(next_x>=0 && next_x<(config.cols -this.shape_width ))this.x  = next_x;
-        if(next_y>=0 && next_y<=(config.rows-this.shape_high))this.y = next_y;
+            
+      let _x=0;
+      let _y=0;
+      if(keyIsDown(LEFT_ARROW)){
+        _x-=1;
       }
+      else if(keyIsDown(RIGHT_ARROW)){
+        _x+=1;
+      }
+      else if(keyIsDown(UP_ARROW)){
+          if(this.angle==270)this.angle=0
+          else this.angle+=90;
+      }
+      else if(keyIsDown(DOWN_ARROW)){
+          _y+=1;
+      }
+      let next_x = parseInt(this.x + _x);
+      let next_y =  parseInt(this.y + _y); 
+      if(next_x>=0 && next_x<(config.cols -this.shape_cols ))this.x  = next_x;
+      if(next_y>=0 && next_y<=(config.rows-this.shape_rows))this.y = next_y;
+      
     }
 }
 
@@ -94,73 +149,73 @@ class Piece
 
 class SquarePiece extends Piece
 {
-  constructor(x,y,current,angle,speed){
+  constructor(x,y,angle,speed){
     let shape = "##\r\n"
                +"##" 
-    super(x,y,current,angle,speed,shape,'green');
+    super(x,y,angle,speed,shape,'green');
   }
 
 }
 class TPiece extends Piece
 {
-  constructor(x,y,current,angle,speed){
-    let shape = "#\r\n"
+  constructor(x,y,angle,speed){
+    let shape = "# \r\n"
                +"##\r\n"
-               +"#" 
-    super(x,y,current,angle,speed,shape,'blue');
+               +"# " 
+    super(x,y,angle,speed,shape,'blue');
   }  
 }
 class StickPiece extends Piece
 {
-  constructor(x,y,current,angle,speed){
+  constructor(x,y,angle,speed){
     let shape = "#\r\n"
                +"#\r\n"
                +"#\r\n"
                +"#" 
-    super(x,y,current,angle,speed,shape,'fucsia');
+    super(x,y,angle,speed,shape,'fucsia');
   }  
 }
 class LPiece extends Piece
 {
-  constructor(x,y,current,angle,speed){
-    let shape = "#\r\n"
-               +"#\r\n"
+  constructor(x,y,angle,speed){
+    let shape = "# \r\n"
+               +"# \r\n"
                +"##"
-    super(x,y,current,angle,speed,shape,'yellow');
+    super(x,y,angle,speed,shape,'yellow');
   }  
 }
 class LLPiece extends Piece
 {
-  constructor(x,y,current,angle,speed){
+  constructor(x,y,angle,speed){
     let shape = "##\r\n"
-               +"#\r\n"
-               +"#"
-    super(x,y,current,angle,speed,shape,'pink');
+               +"# \r\n"
+               +"# "
+    super(x,y,angle,speed,shape,'pink');
   }  
 }
 class T2Piece extends Piece
 {
-  constructor(x,y,current,angle,speed){
+  constructor(x,y,angle,speed){
     let shape = "##\r\n"
-               +"#\r\n"
-               +"#"
-    super(x,y,current,angle,speed,shape,'pink');
+               +"# \r\n"
+               +"# "
+    super(x,y,angle,speed,shape,'pink');
   }  
 }
 class ZPiece extends Piece
 {
-  constructor(x,y,current,angle,speed){
-    let shape = "##\r\n"
+  constructor(x,y,angle,speed){
+    let shape = "## \r\n"
                +" ##"
-    super(x,y,current,angle,speed,shape,'cyan');
+    super(x,y,angle,speed,shape,'cyan');
   }  
 }
 class SPiece extends Piece
 {
-  constructor(x,y,current,angle,speed){
+  constructor(x,y,angle,speed){
     let shape = " ##\r\n"
-               +"##"
-    super(x,y,current,angle,speed,shape,'orange');
+               +"## "
+    super(x,y,angle,speed,shape,'orange');
   }  
 }
 
